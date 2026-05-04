773c5fec849e7f41a7b268bcf6b64b81a582f6e7
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2026-05-03 18:07:08 -0700

    save progress

---
+++ test/test_check_functions.ml
@@ -0,0 +1,18 @@
+open! Core
+open! Desmos_compiler
+open! Languages
+open! Types
+
+(* TODO: make these test cases *)
+
+(* 
+
+if statements are fine if there is a return at the very end
+
+if (x > 1) then
+  reutrn 1
+else
+  x = 2
+
+return 2
+ *)
